30f194e11ad46c0b07897da0b4ad6e2f7a35acb7
xzneozx96 <tuananhngyen218@gmail.com>
2022-03-02 16:31:40 +0700

    config cors for multiple domains

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,18 @@ const messageRoutes = require("./routes/message");
 // middlewares registration
 
 // cross origin resource sharing
+const allowedDomains = [config.deployedDomain, config.localDomain];
 const corsOptions = {
-  origin: config.localDomain,
+  origin: function (origin, callback) {
+    // bypass the requests with no origin (like curl requests, mobile apps, etc )
+    if (!origin) return callback(null, true);
+
+    if (allowedDomains.indexOf(origin) === -1) {
+      const msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true, //access-control-allow-credentials:true
 };
 app.use(cors(corsOptions));
